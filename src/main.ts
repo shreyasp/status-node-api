4f3a5580ae3da2c0a70f92dead778e18b6deb901
shrpatil <shrpatil@redhat.com>
2018-08-17 16:11:00 +0530

    [Minor] Adding Swagger for generating Docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Status App REST API')
+    .setDescription('REST API for Status App consumers')
+    .setVersion('0.0.1')
+    .addTag('Status')
+    .setContactEmail('ektelesi.dev@gmail.com')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
