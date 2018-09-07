f08dc025090aef7fe506dd7feb496c32120963bd
Shreyas Patil <shreyaspatil87@gmail.com>
2018-09-07 15:56:00 +0530

    Sqaush this

--- src/modules/TemplateImage/TemplateImage.service.ts
+++ src/modules/TemplateImage/TemplateImage.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AsyncResultCallback, auto as asyncAuto, each as asyncEach, ErrorCallback } from 'async';
 import { Credentials, S3 } from 'aws-sdk';
 import { Repository } from 'typeorm';
-import { forEach as lodashForEach } from 'lodash';
+import { forEach as lodashForEach, replace as lodashReplace } from 'lodash';
 
 import { assumeS3Role, putS3Object } from '../../utils/aws-s3.utils';
 import { Image } from './TemplateImage.entity';
@@ -72,9 +72,11 @@ class ImageService {
             'test-sts-role-bucket',
         );
 
+        const imageName = lodashReplace(image.originalname, ' ', '');
+
         const s3Uploader: S3 = new S3({credentials: this.tempCredentials});
         const data = await putS3Object(s3Uploader, 'ap-south-1', 'test-sts-role-bucket',
-            `images/${image.originalname}`, image.buffer);
+            `images/${imageName}`, image.buffer);
 
         return new Promise((resolve, reject) => {
             if (!data.success) {
