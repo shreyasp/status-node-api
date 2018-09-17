ab14908e4ab9aa3c5bf212b8ee9a6b0e566a3420
Shreyas Patil <shreyasp@users.noreply.github.com>
2018-09-17 17:50:43 +0530

    [Major] Image Editing API works, upload Image still to be completed

--- src/modules/TemplateEdit/TemplateEdit.service.ts
+++ src/modules/TemplateEdit/TemplateEdit.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { each as asyncEach } from 'async';
 import { createCanvas, loadImage, registerFont } from 'canvas';
 import { createWriteStream } from 'fs';
-import { forEach, get, map, pickBy, split, uniq } from 'lodash';
+import { get, map, pickBy, uniq } from 'lodash';
 import { join } from 'path';
 
 interface IFont {
@@ -53,66 +54,69 @@ interface TemplateLayerNameObj {
 class EditImageService {
   constructor() {}
 
-  getFontFamilyWeight(font: IFont | string): string[] {
-    if (typeof font === 'string') {
-      return split(font, '-');
-    } else {
-      return split(font.fontName, '-');
-    }
-  }
-
-  registerFont(uniqFonts: string[]) {
+  registerTemplateFonts(uniqFonts: string[]) {
     const basePath: string = join(__dirname, 'fonts');
-
-    forEach(uniqFonts, (fontName: string) => {
-      const _font_fam_wt: string[] = this.getFontFamilyWeight(fontName);
-      registerFont(
-        join(basePath, `${fontName}.ttf`),
-        { family: _font_fam_wt[0] },
-        { weight: _font_fam_wt[1] },
-      );
+    asyncEach(uniqFonts, (fontName: string) => {
+      registerFont(join(basePath, `${fontName}.ttf`), { family: `${fontName}` });
     });
   }
 
-  placeTextLayer(layer: ILayer, context: any) {
-    // Update Font in the context
-    const font: IFont = layer.font;
-    const _font_fam_wt: string[] = this.getFontFamilyWeight(font);
-    context.font = `${_font_fam_wt[1]} normal ${font.fontSize} ${_font_fam_wt[0]}`;
-    context.fillText(layer.text, layer.frame.x, layer.frame.y);
+  async placeTextLayers(
+    layers: Partial<TemplateLayerNameObj>,
+    xPlacement: number,
+    context: any,
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {
+      asyncEach(layers, layer => {
+        if (layer.type !== 'image') {
+          const font: IFont = layer.font;
+
+          context.font = `${font.fontSize}px ${layer.font.fontName}`;
+          context.fillStyle = layer.style.color;
+          context.textBaseline = 'top';
+          context.textAlign = layer.alignment;
+
+          try {
+            context.fillText(layer.text, xPlacement, layer.frame.y);
+          } catch (err) {
+            reject(err);
+          }
+        }
+      });
+      resolve(true);
+    });
   }
 
-  editImage(imageMetadata: Partial<TemplateLayerNameObj>) {
+  async editImage(imageMetadata: Partial<TemplateLayerNameObj>): Promise<any> {
     const imageFrame: IFrame = get(imageMetadata, 'image.frame');
     const canvas = createCanvas(imageFrame.width, imageFrame.height);
     const context = canvas.getContext('2d');
 
-    // Get all the unique fonts used in the Tenplate
+    /* Get all the unique fonts used in the Tenplate */
     const uniqFonts: string[] = uniq(
-      map(
-        pickBy(imageMetadata, (o: ILayer) => o.type === 'text'),
-        (v: ILayer) => v.font.fontName,
-      ),
+      map(pickBy(imageMetadata, (o: ILayer) => o.type === 'text'), (v: ILayer) => v.font.fontName),
     );
 
-    // Register Fonts required by the template
-    this.registerFont(uniqFonts);
+    /* Register Fonts required by the template */
+    this.registerTemplateFonts(uniqFonts);
 
-    /* Step - 1: Get the image and createCanvas for it */
-    loadImage(__dirname + '/image/Image.png').then(image => {
-      context.drawImage(
-        image,
-        imageFrame.x,
-        imageFrame.y,
-        imageFrame.width,
-        imageFrame.height,
-      );
-      const noteHeader: ILayer = get(imageMetadata, 'note_header_text');
-      this.placeTextLayer(noteHeader, context);
-      const oStream = createWriteStream(__dirname + '/image/edited.png');
-      const pngStream = canvas.createPNGStream();
-      pngStream.pipe(oStream);
-      oStream.on('finish', () => console.log('New Image Created'));
+    /* Get the image and createCanvas for it */
+    return new Promise((resolve, reject) => {
+      loadImage(__dirname + '/images/Image.png').then(image => {
+        context.drawImage(image, imageFrame.x, imageFrame.y, imageFrame.width, imageFrame.height);
+
+        const xPlacement = imageFrame.width / 2;
+        return this.placeTextLayers(imageMetadata, xPlacement, context)
+          .then(() => {
+            const oStream = createWriteStream(__dirname + '/images/edited.png');
+            const pngStream = canvas.createPNGStream();
+            pngStream.pipe(oStream);
+            oStream.on('finish', () =>
+              resolve({ success: true, message: 'Image created successfully' }),
+            );
+          })
+          .catch(err => reject(err));
+      });
     });
   }
 }
