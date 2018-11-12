88b46d0619541070a159d02c375b91d8bfcb780a
Shreyas Patil <shreyasp@users.noreply.github.com>
2018-11-12 18:50:23 +0530

    [Minor] Updated AWS configuration to be fetched from config.json

--- src/modules/TemplateFont/TemplateFont.service.ts
+++ src/modules/TemplateFont/TemplateFont.service.ts
@@ -11,6 +11,7 @@ import { split } from 'lodash';
 import { DeepPartial, Repository } from 'typeorm';
 
 import { assumeS3Role, putS3Object } from '../../utils/aws-s3.utils';
+import { AppConfigService } from '../AppConfig/AppConfig.service';
 import { Font } from './TemplateFont.entity';
 
 @Injectable()
@@ -20,6 +21,7 @@ class FontService {
   // Temporary credentials to be used for uploading font object
   // to S3.
   tempCredentials: Credentials;
+  config = new AppConfigService().readAppConfig();
 
   // Retrieve all the active Font
   findAllFonts() {
@@ -112,19 +114,19 @@ class FontService {
   async uploadFontToS3(font: any): Promise<any> {
     return new Promise((resolve, reject) => {
       assumeS3Role(
-        '369329776707',
-        'Test',
-        'S3-Upload-Session',
-        'ap-south-1',
-        's3:PutObject',
-        'test-sts-role-bucket',
+        `${this.config.accountId}`,
+        `${this.config.assumedRole}`,
+        `s3-font-upload`,
+        `${this.config.awsRegion}`,
+        's3:*',
+        `${this.config.bucketName}`,
       )
         .then(credentials => {
           const s3Uploader: S3 = new S3({ credentials });
           putS3Object(
             s3Uploader,
-            'ap-south-1',
-            'test-sts-role-bucket',
+            `${this.config.awsRegion}`,
+            `${this.config.bucketName}`,
             `fonts/${font.originalname}`,
             font.buffer,
           )
