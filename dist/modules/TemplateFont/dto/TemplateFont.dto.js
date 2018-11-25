bd9d516112abf1eca5f01ed558ffec35beca1083
Shreyas Patil <shreyasp@users.noreply.github.com>
2018-11-25 11:31:43 +0530

    Using Cloudfront URL for caching Image and fonts from S3. Minor tweaks and bug fixes

--- dist/modules/TemplateFont/dto/TemplateFont.dto.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-class TemplateFontDTO {}
-exports.TemplateFontDTO = TemplateFontDTO;
-//# sourceMappingURL=TemplateFont.dto.js.map
