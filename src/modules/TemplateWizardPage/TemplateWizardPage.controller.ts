a8ec70ca3c6b2e20fb09581e6fbbc246c5e42701
Shreyas Patil <shreyasp@users.noreply.github.com>
2019-06-13 09:47:39 +0530

    [Partial] Implementation Layer master and Wizard Page relationship

--- src/modules/TemplateWizardPage/TemplateWizardPage.controller.ts
+++ src/modules/TemplateWizardPage/TemplateWizardPage.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 
 import { WizardPageService } from './TemplateWizardPage.service';
 
 @Controller('wizard-page')
 class WizardPageController {
   constructor(private readonly wizardPageService: WizardPageService) {}
+
+  @Get()
+  getAllWizardPagesByCategory(@Query() query): object {
+    return this.wizardPageService.getAllWizardPagesByCategory(query.category);
+  }
 }
 
 export { WizardPageController };
--- src/modules/TemplateWizardPage/TemplateWizardPage.service.ts
+++ src/modules/TemplateWizardPage/TemplateWizardPage.service.ts
@@ -1,14 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { isEmpty } from 'lodash';
+import { DeepPartial, Repository } from 'typeorm';
 
+import { Category } from '../TemplateCategory/TemplateCategory.entity';
 import { WizardPage } from './TemplateWizardPage.entity';
 
 @Injectable()
 class WizardPageService {
   constructor(
-    @InjectRepository(WizardPage) private readonly WizardPageRepository: Repository<WizardPage>,
+    @InjectRepository(WizardPage) private readonly wizardPageRepository: Repository<WizardPage>,
   ) {}
+
+  getAllWizardPagesByCategory(category: DeepPartial<Category>) {
+    const queryBuilder = this.wizardPageRepository.createQueryBuilder('wizardPage');
+    return queryBuilder
+      .innerJoinAndSelect('wizardPage.category', 'category', 'wizardPage.category = :category', {
+        category,
+      })
+      .where({ isActive: true })
+      .getManyAndCount()
+      .then(data => {
+        const wizardPages = data[0];
+        const totalWizardPages = data[1];
+
+        if (isEmpty(wizardPages)) {
+          return {
+            success: true,
+            message: 'No wizard pages present for given category in the database',
+            data: {
+              wizardPages,
+              totalWizardPages,
+            },
+          };
+        }
+      })
+      .catch(err => ({
+        success: false,
+        message: 'Something went wrong while trying to retrieve wizard pages',
+        err,
+      }));
+  }
 }
 
 export { WizardPageService };
