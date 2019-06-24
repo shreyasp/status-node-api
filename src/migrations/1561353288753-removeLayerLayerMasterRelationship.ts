c6313de572bb5e765ccc9a00d601f061b0944cfb
shrpatil <shrpatil@redhat.com>
2019-06-24 10:34:39 +0530

    Removed migrations for correcting the layer layer-master relation

--- src/migrations/1561353288753-removeLayerLayerMasterRelationship.ts
+++ src/migrations/1561353288753-removeLayerLayerMasterRelationship.ts
@@ -1,7 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class layerLayerMasterIdNumber1561292321166 implements MigrationInterface {
+export class removeLayerLayerMasterRelationship1561353288753 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`ALTER TABLE "layer" DROP CONSTRAINT "FK_4183f15ac4502b4fae4f025b483"`);
+    await queryRunner.query(`ALTER TABLE "layer" DROP COLUMN "layerMasterEntId"`);
+    await queryRunner.query(`ALTER TABLE "layer" DROP COLUMN "layerMasterLayerMasterId"`);
+    await queryRunner.query(`ALTER TABLE "layer" ADD "layerMasterId" integer NOT NULL`);
     await queryRunner.query(`ALTER TABLE "layer_frame" DROP COLUMN "height"`);
     await queryRunner.query(`ALTER TABLE "layer_frame" ADD "height" float NOT NULL DEFAULT 0`);
     await queryRunner.query(`ALTER TABLE "layer_frame" DROP COLUMN "width"`);
@@ -32,6 +36,12 @@ export class layerLayerMasterIdNumber1561292321166 implements MigrationInterface
     await queryRunner.query(`ALTER TABLE "layer_frame" DROP COLUMN "height"`);
     await queryRunner.query(
       `ALTER TABLE "layer_frame" ADD "height" double precision NOT NULL DEFAULT 0`,
+    );
+    await queryRunner.query(`ALTER TABLE "layer" DROP COLUMN "layerMasterId"`);
+    await queryRunner.query(`ALTER TABLE "layer" ADD "layerMasterLayerMasterId" integer`);
+    await queryRunner.query(`ALTER TABLE "layer" ADD "layerMasterEntId" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "layer" ADD CONSTRAINT "FK_4183f15ac4502b4fae4f025b483" FOREIGN KEY ("layerMasterEntId", "layerMasterLayerMasterId") REFERENCES "layer_master"("EntId","layerMasterId") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
   }
 }
