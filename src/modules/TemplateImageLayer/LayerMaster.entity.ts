30f6cbbf56262f5f501bde71b8cf506b75fbd263
shrpatil <shrpatil@redhat.com>
2019-07-10 10:39:39 +0530

    Removed unused imports

--- src/modules/TemplateImageLayer/LayerMaster.entity.ts
+++ src/modules/TemplateImageLayer/LayerMaster.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 import { CommonEntity } from '../../entities/common.entity';
 import { WizardPage } from '../TemplateWizardPage/TemplateWizardPage.entity';
